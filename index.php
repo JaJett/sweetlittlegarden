a1e6af45f65dae3e095dcbee35d61729514c45db
JaJett <riakimuhammad402@gmail.com>
2024-12-20 16:02:03 +0800

    Terbaru

--- index.php
+++ index.php
@@ -101,7 +101,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
         <div class="container">
             <a class="navbar-brand" href="#">
-                <img src="photo-content/logo.JPEG" alt="Sweet Little Garden"> 
+                <img src="photo-content/logo.jpeg" alt="Sweet Little Garden"> 
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
